94b8be59a154545ab95a09a34331ba0d2ccbaf5a
Skulkrone <40754599+Skulkrone@users.noreply.github.com>
2022-11-01 16:38:49 +0100

    first commit

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './Navbar.module.css'
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Link legacyBehavior href="/">
+        <a>Accueil</a>
+      </Link>
+      <Link legacyBehavior href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link legacyBehavior href="/contact">
+        <a>Contact</a>
+      </Link>
+    </nav>
+  )
+}
